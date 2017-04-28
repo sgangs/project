984d6add06b62314370e1d57e6c26aefb0164e46
Sayantan Ganguly <sayantangangs.91@gmail.com>
2017-04-28 21:07:54 +0530

    Reflect delete

--- school/school_accountentry/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchoolAccountentryConfig(AppConfig):
-    name = 'school_accountentry'
--- school/school_user/static/jsontable.js
+++
@@ -1,5 +0,0 @@
-$('.test').click( function() {
-  var table = $('#inventory').tableToJSON();
-  console.log(table);
-  alert(JSON.stringify(table));  
-});
